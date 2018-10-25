2768ac57aed9e95cd80d500242c18584d5b326e5
LilliamHaro <qualia950@gmail.com>
2018-10-25 18:01:37 -0500

    msk-slider

--- main.js
+++ main.js
@@ -5,9 +5,35 @@ $(document).ready(function() {
     $(".nav-collapse-btn span:nth-child(3)").toggleClass("span-cross-3");
     $(".container-bg").toggleClass("container-bg-expand");
     setTimeout(function() {
-          $(".container-bg").toggleClass("nav-transition-out");
-          $('.container-bg ul li').toggleClass('container-bg-ul-li-close')
-        }, 800);
+      $(".container-bg").toggleClass("nav-transition-out");
+      $(".container-bg ul li").toggleClass("container-bg-ul-li-close");
+    }, 800);
     $(".container-bg ul li").toggleClass("nav-ul-li-showed");
   });
 });
+
+
+setTimeout(function() {
+  $(".slider-3").addClass("impar");
+}, 1500);
+setTimeout(function() {
+  $(".slider-2").addClass("par");
+}, 4500);
+setTimeout(function() {
+  $(".slider-3").removeClass("impar");
+  $(".slider-2").removeClass("par");
+}, 6000);
+
+setInterval(function(){ 
+  setTimeout(function() {
+    $(".slider-3").addClass("impar");
+  }, 1500);
+  setTimeout(function() {
+    $(".slider-2").addClass("par");
+  }, 4500);
+  setTimeout(function() {
+    $(".slider-3").removeClass("impar");
+    $(".slider-2").removeClass("par");
+  }, 6000);
+ },7000);
+
